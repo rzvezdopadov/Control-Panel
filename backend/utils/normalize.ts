558db350cfdfbc05388c14276adfb9a248046c77
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-17 10:37:20 +0400

    added nomalize util

---
+++ backend/utils/normalize.ts
@@ -0,0 +1,32 @@
+export const normalize = {
+	deleteSpace(str: string): string {
+		if (!str) return "";
+		if (typeof str !== "string") return "";
+
+		str = str.replace(/[' ']/g, "");
+
+		return str;
+	},
+	deleteEmptyString(arr: String[]) {
+		const newArr: String[] = [];
+
+		arr.forEach((str) => {
+			if (typeof str === "string") {
+				newArr.push(normalize.deleteSpace(str));
+			}
+		});
+
+		return newArr;
+	},
+	number(num: number): number {
+		const type = typeof num;
+
+		if (!(type === "number" || type === "string")) return 0;
+
+		const newNum = Number(num);
+
+		if (Number.isNaN(newNum)) return 0;
+
+		return newNum;
+	},
+};
