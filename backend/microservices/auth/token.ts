9b53bab1e1dbc5aec4c18652308c61d2b1a72ee4
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-21 14:29:21 +0400

    added utils token

---
+++ backend/microservices/auth/token.ts
@@ -0,0 +1,49 @@
+import { normalize } from "../../utils/normalize";
+import { authDB } from "./authDB";
+
+const config = require("config");
+const jwtToken = require("jsonwebtoken");
+
+interface IdecodeJWT {
+	userId: string;
+	iat: number;
+	exp: number;
+}
+
+export function testOverflowJWT(jwtExp: number = 0) {
+	if (Math.round(Date.now() / 1000) > jwtExp) return true;
+
+	return false;
+}
+
+export async function testToken(jwt: string) {
+	if (!jwt) return false;
+
+	try {
+		const decode = (await jwtToken.verify(jwt, config.get("jwtSecret"))) as IdecodeJWT;
+
+		if (testOverflowJWT(decode.exp)) {
+			return false;
+		}
+
+		const userId = decode.userId;
+
+		const token = await authDB.getJWT(userId);
+
+		if (!token || !token.length) return false;
+
+		for (let i = 0; i < token.length; i++) {
+			if (token[i].token === jwt) {
+				decode.userId = normalize.deleteSpace(decode.userId);
+
+				return decode;
+			}
+		}
+
+		return false;
+	} catch (error) {
+		console.log("testToken", error);
+
+		return false;
+	}
+}
