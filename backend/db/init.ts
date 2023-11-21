2f12f1018be7bc89e00ea49dfa9ed8f35ca30502
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-21 14:31:07 +0400

    added init

--- backend/db/init.ts
+++ backend/db/init.ts
@@ -1,10 +1,12 @@
 import { initDBAlarm } from "./initdb/alarm/alarmDBInit";
 import { initDBAuth } from "./initdb/auth/authDBInit";
+import { initDBProfile } from "./initdb/profile/profileDBInit";
 
 export async function initDB(): Promise<boolean> {
 	try {
 		await initDBAuth();
 		await initDBAlarm();
+		await initDBProfile();
 
 		return true;
 	} catch (error) {
