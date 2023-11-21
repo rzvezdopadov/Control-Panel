d26379c72d95b1789be179608b89093a65921633
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-21 14:19:57 +0400

    added random lib

---
+++ utils/random.test.ts
@@ -0,0 +1,20 @@
+import { Random } from "./random";
+
+const testArrClassic: Array<number> = [];
+for (let i = 0; i < 3; i++) testArrClassic.push(1 + Math.floor(Math.random() * 30));
+
+describe.each(testArrClassic)("getRandomString classic test", (value) => {
+	it(`input: ${value}`, () => {
+		const str = Random.getRandomString(value);
+		expect(str.length).toBe(value);
+		expect(typeof str).toBe("string");
+	});
+});
+
+const testArrSpecial = [Math.floor(Math.random() * 30) - 50, [], {}, null, undefined, Infinity];
+
+describe.each(testArrSpecial)("getRandomString special test", (value) => {
+	test(`input: ${value}`, () => {
+		expect(Random.getRandomString(Number(value))).toBe("");
+	});
+});
