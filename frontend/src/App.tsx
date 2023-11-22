71c54304379d82f08c9df9ba43d08fd9a42fccf2
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-22 15:09:17 +0400

    integrated new component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { AppHeader } from "./components/selections/AppHeader";
+import { AppMain } from "./components/selections/AppMain";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<AppHeader />
+			<AppMain />
+		</div>
+	);
 }
 
 export default App;
