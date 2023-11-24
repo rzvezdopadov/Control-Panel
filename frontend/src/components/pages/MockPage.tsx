96a0b02904d0b6588408f62f4c1be160a2b28eec
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-24 13:04:24 +0400

    added mock page

---
+++ frontend/src/components/pages/MockPage.tsx
@@ -0,0 +1,5 @@
+import { MainScrollWrapper } from "../wrappers/MainScrollWrapper";
+
+export function MockPage() {
+	return <MainScrollWrapper center={true} color={true} shadow={true}></MainScrollWrapper>;
+}
