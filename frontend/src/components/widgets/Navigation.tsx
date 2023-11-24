437797609e1610f6c5f734e31771d71f5601b567
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-24 13:02:59 +0400

    change bl navigation

--- frontend/src/components/widgets/Navigation.tsx
+++ frontend/src/components/widgets/Navigation.tsx
@@ -5,39 +5,25 @@ import { ACCTYPE } from "../../../../global/roles";
 import { ButtonNavigationLink } from "../utils/Buttons/Buttons";
 import { Logout } from "../pages/Logout";
 
-export const linkNoAuth: ILink[] = [
-	{ to: "/", imgSrc: "", title: "Главная" },
-	{ to: "/login", imgSrc: "", title: "Войти" },
-];
-export const linkAuthUser: ILink[] = [
-	{ to: "/", imgSrc: "", title: "Главная" },
-	{ to: "/logout", imgSrc: "", title: "Выход" },
-];
+export const linkNoAuth: ILink[] = [{ to: "/login", imgSrc: "", title: "Войти" }];
+export const linkAuthUser: ILink[] = [{ to: "/logout", imgSrc: "", title: "Выход" }];
 
 export function Navigation(payload: { naviKey: string }) {
 	const { jwt, userMyProfile } = store.getState();
 
 	const NavigationMemo = useMemo(() => {
 		return jwt ? (
-			userMyProfile?.userid !== "" ? (
-				userMyProfile.acctype === ACCTYPE.user ? (
-					<>
-						{linkAuthUser.map((link) => {
-							return (
-								<ButtonNavigationLink
-									link={link}
-									naviKey={payload.naviKey}
-									key={"linkAuthUser" + payload.naviKey + link.to}
-								/>
-							);
-						})}
-					</>
-				) : (
-					<Logout></Logout>
-				)
-			) : (
-				<></>
-			)
+			<>
+				{linkAuthUser.map((link) => {
+					return (
+						<ButtonNavigationLink
+							link={link}
+							naviKey={payload.naviKey}
+							key={"linkAuthUser" + payload.naviKey + link.to}
+						/>
+					);
+				})}
+			</>
 		) : (
 			<>
 				{linkNoAuth.map((link) => {
