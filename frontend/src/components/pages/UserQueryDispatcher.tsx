397f81ea51a57353a8c63885a51211a84693337c
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-24 08:57:18 +0400

    added base page

---
+++ frontend/src/components/pages/UserQueryDispatcher.tsx
@@ -0,0 +1,22 @@
+import { store } from "../../store/store";
+import { Button } from "../utils/Buttons/Buttons";
+import { FormWrapper } from "../wrappers/FormWrapper";
+import { MainScrollWrapper } from "../wrappers/MainScrollWrapper";
+import { Label, LabelWidget } from "../utils/Labels/Labels";
+
+export function UserQueryDispatcher() {
+	const { userMyProfile } = store.getState();
+	const btnUserQueryClickHandler = () => {};
+
+	return (
+		<MainScrollWrapper center={true} color={true} shadow={true}>
+			<FormWrapper>
+				<Label value={`Пользователь: ${userMyProfile.bio}`} />
+				<Label value={`Место: ${userMyProfile.place}`} />
+				<Label value={`Статус вызова: ${userMyProfile.bio}`} />
+				<LabelWidget value={`Вызов диспетчера`} />
+				<Button onClick={btnUserQueryClickHandler} value={`Вызвать`} checked={true} />
+			</FormWrapper>
+		</MainScrollWrapper>
+	);
+}
