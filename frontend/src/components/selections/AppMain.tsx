50c4ac781f300b330fc32549000b110b4aafc7e4
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-24 08:59:09 +0400

    added test routes

--- frontend/src/components/selections/AppMain.tsx
+++ frontend/src/components/selections/AppMain.tsx
@@ -1,5 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import { store } from "../../store/store";
 import { MainWrapper } from "../wrappers/MainWrapper";
+import { Logout } from "../pages/Logout";
+import { Login } from "../pages/Login";
+import { UserQueryDispatcher } from "../pages/UserQueryDispatcher";
 
 export function AppMain() {
-	return <MainWrapper></MainWrapper>;
+	const { jwt, userMyProfile } = store.getState();
+
+	return (
+		<MainWrapper>
+			<Routes>
+				{jwt ? (
+					userMyProfile?.userid !== "" ? (
+						<>
+							<Route path="/logout" element={<Logout />} />
+
+							<Route path="/*" element={<Login />} />
+						</>
+					) : (
+						<Route path="/*" element={<></>} />
+					)
+				) : (
+					<>
+						<Route path="/login" element={<Login />} />
+
+						<Route path="/*" element={<UserQueryDispatcher />} />
+					</>
+				)}
+			</Routes>
+		</MainWrapper>
+	);
 }
