5f4eb2b6fc3a3efa03c7cb5eea10d3a3563b0d9a
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-24 09:26:00 +0400

    added shop interface

---
+++ global/interfaces/ishop.ts
@@ -0,0 +1,30 @@
+export interface IRowShop {
+	Row1: false;
+	Row2: false;
+	Row3: false;
+	Row4: false;
+	Row5: false;
+}
+
+export interface IShop {
+	Col1: IRowShop;
+	Col2: IRowShop;
+	Col3: IRowShop;
+	Col4: IRowShop;
+	Col5: IRowShop;
+	Col6: IRowShop;
+	Col7: IRowShop;
+	Col8: IRowShop;
+	Col9: IRowShop;
+	Col10: IRowShop;
+	Col11: IRowShop;
+	Col22: IRowShop;
+	Col23: IRowShop;
+	Col24: IRowShop;
+	Col25: IRowShop;
+	Col26: IRowShop;
+	Col27: IRowShop;
+	Col28: IRowShop;
+	Col29: IRowShop;
+	Col30: IRowShop;
+}
