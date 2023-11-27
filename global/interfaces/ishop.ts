d224e340640bfc529bf6896e1a66ed4e9acee796
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-27 15:16:16 +0400

    change structure state

--- global/interfaces/ishop.ts
+++ global/interfaces/ishop.ts
@@ -1,30 +1,89 @@
-export interface IRowShop {
-	Row1: false;
-	Row2: false;
-	Row3: false;
-	Row4: false;
-	Row5: false;
-}
-
 export interface IShop {
-	Col1: IRowShop;
-	Col2: IRowShop;
-	Col3: IRowShop;
-	Col4: IRowShop;
-	Col5: IRowShop;
-	Col6: IRowShop;
-	Col7: IRowShop;
-	Col8: IRowShop;
-	Col9: IRowShop;
-	Col10: IRowShop;
-	Col11: IRowShop;
-	Col22: IRowShop;
-	Col23: IRowShop;
-	Col24: IRowShop;
-	Col25: IRowShop;
-	Col26: IRowShop;
-	Col27: IRowShop;
-	Col28: IRowShop;
-	Col29: IRowShop;
-	Col30: IRowShop;
+	1.1: boolean;
+	1.2: boolean;
+	1.3: boolean;
+	1.4: boolean;
+
+	2.1: boolean;
+	2.2: boolean;
+	2.3: boolean;
+	2.4: boolean;
+
+	3.1: boolean;
+	3.2: boolean;
+	3.3: boolean;
+	3.4: boolean;
+	3.5: boolean;
+
+	4.1: boolean;
+	4.2: boolean;
+	4.3: boolean;
+	4.4: boolean;
+
+	5.1: boolean;
+	5.2: boolean;
+	5.3: boolean;
+	5.4: boolean;
+
+	6.1: boolean;
+	6.2: boolean;
+	6.3: boolean;
+	6.4: boolean;
+
+	7.1: boolean;
+	7.2: boolean;
+	7.3: boolean;
+	7.4: boolean;
+
+	8.1: boolean;
+	8.2: boolean;
+	8.3: boolean;
+	8.4: boolean;
+	8.5: boolean;
+
+	9.1: boolean;
+	9.2: boolean;
+	9.3: boolean;
+	9.4: boolean;
+	9.5: boolean;
+
+	10.1: boolean;
+	10.2: boolean;
+	10.3: boolean;
+	10.4: boolean;
+
+	22.1: boolean;
+	22.2: boolean;
+	22.3: boolean;
+
+	23.1: boolean;
+	23.2: boolean;
+	23.3: boolean;
+
+	24.1: boolean;
+	24.2: boolean;
+	24.3: boolean;
+
+	25.1: boolean;
+	25.2: boolean;
+	25.3: boolean;
+
+	26.1: boolean;
+	26.2: boolean;
+
+	27.1: boolean;
+	27.2: boolean;
+	27.3: boolean;
+
+	28.1: boolean;
+	28.2: boolean;
+	28.3: boolean;
+
+	29.1: boolean;
+	29.2: boolean;
+	29.3: boolean;
+
+	30.1: boolean;
+	30.2: boolean;
+	30.3: boolean;
 }
