b6376253025e00dd812753e4c09630769cc882e5
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-28 15:48:30 +0400

    integrated base table

--- frontend/src/components/pages/AdminPanel.tsx
+++ frontend/src/components/pages/AdminPanel.tsx
@@ -1,15 +1,47 @@
 import { store } from "../../store/store";
-import { FormWrapper } from "../wrappers/FormWrapper";
 import { MainScrollWrapper } from "../wrappers/MainScrollWrapper";
 import { LabelWidget } from "../utils/Labels/Labels";
+import { useEffect, useState } from "react";
+import { IProfile } from "../interfaces/iprofiles";
+import { useQueryGetProfiles } from "../../api/profile/profile.api.hook";
+import { modalMessageOpen } from "../modal/ModalMessage";
+import { ACCTYPE } from "../../../../global/roles";
+import { Table } from "../utils/Tables/Tables";
+import { Button } from "../utils/Buttons/Buttons";
 
 export function AdminPanel() {
 	const { userMyProfile } = store.getState();
+	const [profiles, setProfiles] = useState<IProfile[]>([]);
+
+	const { dataGetProfiles, errorGetProfiles, querySendGetProfiles } = useQueryGetProfiles();
+
+	useEffect(() => {
+		querySendGetProfiles({
+			acctype: ACCTYPE.user,
+		});
+	}, []);
+
+	useEffect(() => {
+		if (!dataGetProfiles) return;
+
+		setProfiles(dataGetProfiles);
+	}, [dataGetProfiles]);
+
+	useEffect(() => {
+		if (!errorGetProfiles) return;
+
+		modalMessageOpen(errorGetProfiles.response.data.message);
+	}, [errorGetProfiles]);
 
 	return (
-		<MainScrollWrapper center={true} color={true} shadow={true}>
+		<MainScrollWrapper color={true} shadow={true}>
 			<LabelWidget value={`Администратор: ${userMyProfile.bio}`} />
-			<FormWrapper></FormWrapper>
+			<Button value={`Добавить пользователя`} />
+			<Table
+				headTitle={["Место", "ФИО", "Логин", "Тип аккаунта", "userID"]}
+				propertySeq={["place", "bio", "login", "acctype", "userid"]}
+				data={dataGetProfiles}
+			></Table>
 		</MainScrollWrapper>
 	);
 }
