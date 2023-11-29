a637fedc8c00a18df67ee70b0897cee6909aac77
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-29 16:17:33 +0400

    create modal wrapper

---
+++ frontend/src/components/wrappers/ModalFormWrapper.tsx
@@ -0,0 +1,35 @@
+import React, { MouseEventHandler } from "react";
+import { ButtonClose } from "../utils/Buttons/Buttons";
+import { useFormVisible } from "../../hooks/form.hook";
+
+export function ModalCloseFormWrapper(payload: {
+	children?: React.ReactNode;
+	enabled?: boolean;
+	formVisible?: ReturnType<typeof useFormVisible>;
+}) {
+	const closeModalHandler = () => {
+		if (!payload.formVisible?.setVisible) return;
+
+		payload.formVisible.setVisible(false);
+	};
+
+	return (
+		<div
+			onClick={closeModalHandler}
+			ref={payload.formVisible?.refDivVisible}
+			className="flex invisible fixed top-0 left-0 bg-opacity-90 bg-gray-900 z-50 w-full h-full"
+		>
+			<div className="flex flex-col relative pb-2 rounded-2xl border-2 border-lime-500 m-auto">
+				<ButtonClose title="Закрыть" onClick={closeModalHandler}></ButtonClose>
+				<div
+					className="flex px-12 py-2 flex-col"
+					onClick={(e) => {
+						e.stopPropagation();
+					}}
+				>
+					{payload.children ? payload.children : <></>}
+				</div>
+			</div>
+		</div>
+	);
+}
