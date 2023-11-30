6d6635dd1bfac74540397ddbbe9497dcd2716a2e
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-30 10:04:54 +0400

    change layout

--- frontend/src/components/wrappers/ModalFormWrapper.tsx
+++ frontend/src/components/wrappers/ModalFormWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from "react";
+import React from "react";
 import { ButtonClose } from "../utils/Buttons/Buttons";
 import { useFormVisible } from "../../hooks/form.hook";
 
@@ -19,7 +19,7 @@ export function ModalCloseFormWrapper(payload: {
 			ref={payload.formVisible?.refDivVisible}
 			className="flex invisible fixed top-0 left-0 bg-opacity-90 bg-gray-900 z-50 w-full h-full"
 		>
-			<div className="flex flex-col relative pb-2 rounded-2xl border-2 border-lime-500 m-auto">
+			<div className="flex flex-col relative bg-inherit pb-2 rounded-2xl border-2 border-lime-500 m-auto">
 				<ButtonClose title="Закрыть" onClick={closeModalHandler}></ButtonClose>
 				<div
 					className="flex px-12 py-2 flex-col"
