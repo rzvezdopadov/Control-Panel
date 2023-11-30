133ad0d7bff8141bef2df967afd4ca96ac0cb31e
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-11-30 10:17:16 +0400

    fix problem with key

--- frontend/src/components/utils/Tables/Tables.tsx
+++ frontend/src/components/utils/Tables/Tables.tsx
@@ -13,11 +13,11 @@ export function Table(payload: { headTitle: string[]; propertySeq: string[]; dat
 			<tbody className="[&>*:nth-child(2n+2)]:bg-slate-600">
 				{payload?.data ? (
 					payload.data.map((obj) => (
-						<tr key={`tbody${obj}`}>
+						<tr key={`tbody${JSON.stringify(obj)}`}>
 							{payload.propertySeq.map((value) => (
 								<td
 									className="border-2 p-1 border-lime-400 "
-									key={`tdtr${obj + value}`}
+									key={`trtd${JSON.stringify(obj) + value}`}
 								>
 									{obj[value]}
 								</td>
