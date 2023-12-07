24cb4d73fe97167d02a25dd09826ada4204c5a26
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-12-07 10:23:07 +0400

    added watch

--- frontend/src/components/wrappers/MainScrollWrapper.tsx
+++ frontend/src/components/wrappers/MainScrollWrapper.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ButtonScrollToTop } from "../utils/Buttons/Buttons";
 import { invisibleOnScrollToTop } from "../../helpers/scroll";
+import { TimeDate } from "../../../../utils/timedate";
 
 export function MainScrollWrapper(payload: {
 	children?: React.ReactNode;
@@ -12,6 +13,16 @@ export function MainScrollWrapper(payload: {
 	const scrollTopDiv = React.useRef(null);
 	const scrollToTopBtn = React.useRef(null);
 
+	const [timecode, setTimecode] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => setTimecode(TimeDate.getTimecodeNow()), 800);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
 	return (
 		<div
 			className={`flex flex-col fixed items-center text-neutral-50 rounded-xl overflow-y-scroll top-20 bottom-4 left-0 right-0 m-auto px-2 pt-2 pb-2 z-0${
@@ -22,6 +33,10 @@ export function MainScrollWrapper(payload: {
 			onScroll={(e) => invisibleOnScrollToTop(e, scrollToTopBtn)}
 			ref={scrollTopDiv}
 		>
+			<div className="flex text-2xl w-full justify-center text-white z-10">
+				{timecode ? `${TimeDate.getTimeDateFromTimeCode(timecode)}` : "XX.XX.XXXX XX:XX:XX"}
+			</div>
+
 			<ButtonScrollToTop scrollTopDiv={scrollTopDiv} scrollToTopBtn={scrollToTopBtn} />
 			{payload.children ? payload.children : <></>}
 		</div>
