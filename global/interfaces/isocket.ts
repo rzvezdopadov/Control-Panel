548063bd12f33195a80f722b125f49b44f91de7e
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-12-07 10:21:55 +0400

    added type for socket

---
+++ global/interfaces/isocket.ts
@@ -0,0 +1,9 @@
+export enum SOCKET_COMMAND {
+	ping = "ping",
+	getJWT = "getJWT",
+	getState = "getState",
+	setState = "setState",
+	clearAllPosition = "clearAllPosition",
+	changePosition = "changePosition",
+	disconnect = "disconnect",
+}
